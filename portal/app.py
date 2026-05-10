214afb81cd02af66826b2a5b3acbe2309e25db3c
Eswar2000 <v.eswarraman2000@gmail.com>
2026-05-10 16:42:43 +0530

    updated frontend experience with toasts and spinners

--- portal/app.py
+++ portal/app.py
@@ -6,18 +6,18 @@ st.title("Resume Screening App")
 uploaded_file = st.file_uploader("Upload a resume (PDF)", type="pdf")
 
 if uploaded_file is not None:
-    st.write("File uploaded successfully", uploaded_file.name)
-    print("File uploaded successfully", uploaded_file.name)
+    st.toast(f"File \"{uploaded_file.name}\" uploaded successfully", icon="🎉", duration="short")
 
     if st.button("Process Resume"):
-        response = requests.post(
-            "http://localhost:8000/screening/",
-            files={"resume": uploaded_file}
-        )
+        with st.spinner("Procesing resume..."):
+            response = requests.post(
+                "http://localhost:8000/screening/",
+                files={"resume": uploaded_file}
+            )
 
-        if response.status_code == 200:
-            st.write("Resume processed successfully")
-            response_data = response.json()
-            st.write("Candidate details: ", response_data)
-        else:
-            st.write("Error processing resume")
+            if response.status_code == 200:
+                st.toast("Resume processed successfully", icon="✅", duration="short")
+                response_data = response.json()
+                st.write("Candidate details: ", response_data)
+            else:
+                st.toast("Error processing resume", icon="🚨", duration="short")
